8efdc108248f6b896d121fcf0efc6855f37daed9
Leandro Hanc <leandrohanc@gmail.com>
2023-03-31 16:58:13 -0300

    feat(): add Ornaments component with animations

--- app/[lang]/components/views/Home/Home.tsx
+++ app/[lang]/components/views/Home/Home.tsx
@@ -1,10 +1,20 @@
+// Components
+import Ornaments from "@/app/[lang]/components/views/Home/components/Ornaments";
+
 // Utils
 import { getDictionary } from "@/i18n/get-dictionary";
+
+// Styles
+import styles from "./Home.module.css";
 
 interface HomeProps {
 	dictionary: Awaited<ReturnType<typeof getDictionary>>;
 }
 
 export default function Home({ dictionary }: HomeProps) {
-	return <main>{dictionary["server-component"].welcome}</main>;
+	return (
+		<section className={styles.home}>
+			<Ornaments />
+		</section>
+	);
 }
---
+++ app/[lang]/components/views/Home/components/Ornaments/Ornaments.tsx
@@ -0,0 +1,43 @@
+import classNames from "classnames";
+import NextImage from "next/image";
+
+// Assets
+import Ornament from "@/public/images/ornament.svg";
+
+// Styles
+import styles from "./Ornaments.module.css";
+
+export default function Ornaments() {
+	return (
+		<div aria-hidden className={styles.ornaments}>
+			<NextImage
+				src={Ornament}
+				alt=""
+				priority
+				height={48}
+				className={classNames([styles.ornament, styles["top-left"]])}
+			/>
+			<NextImage
+				src={Ornament}
+				alt=""
+				priority
+				height={48}
+				className={classNames([styles.ornament, styles["top-right"]])}
+			/>
+			<NextImage
+				src={Ornament}
+				alt=""
+				priority
+				height={48}
+				className={classNames([styles.ornament, styles["bottom-left"]])}
+			/>
+			<NextImage
+				src={Ornament}
+				alt=""
+				priority
+				height={48}
+				className={classNames([styles.ornament, styles["bottom-right"]])}
+			/>
+		</div>
+	);
+}
